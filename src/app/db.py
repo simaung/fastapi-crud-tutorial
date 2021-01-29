54b67ff638681f86a04afee9af09ecedbbc4c931
simaung <ddhr00@gmail.com>
2021-01-29 17:37:40 +0700

    models - sqlalchemy model

--- src/app/db.py
+++ src/app/db.py
@@ -1,14 +1,24 @@
 import os
 
-from databases import Database
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import (Column, DateTime, Integer, MetaData, String, Table,
+                        create_engine)
+from sqlalchemy.sql import func
 
+from databases import Database
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
+notes = Table(
+    "notes",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(50)),
+    Column("description", String(50)),
+    Column("created_at", DateTime, default=func.now(), nullable=False),
+)
 
 # database query builder
 database = Database(DATABASE_URL)
