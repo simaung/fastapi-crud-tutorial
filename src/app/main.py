6722000254b15da2e40dad13db486abafa41405e
simaung <ddhr00@gmail.com>
2021-02-01 17:55:15 +0700

    post route

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.api import home
+from app.api import home, notes
 from app.db import engine, metadata, database
 
 metadata.create_all(engine)
@@ -16,3 +16,4 @@ async def shutdown():
     await database.disconnect()
 
 app.include_router(home.router)
+app.include_router(notes.router, prefix="/notes", tags=["notes"])
